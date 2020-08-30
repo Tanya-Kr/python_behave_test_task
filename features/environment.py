7c3e51862bcd05dac7dd41caab3a882c5acf7961
Tanya-Kr <tanya.krug7@gmail.com>
2020-08-30 16:14:44 +0300

    Refactor environment module

--- features/environment.py
+++ features/environment.py
@@ -1,10 +1,11 @@
 from behave import fixture, use_fixture
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @fixture
 def browser_chrome(context):
-    context.browser = webdriver.Chrome()
+    context.browser = webdriver.Chrome(ChromeDriverManager().install())
     context.browser.maximize_window()
     context.browser.implicitly_wait(10)
     yield context.browser
