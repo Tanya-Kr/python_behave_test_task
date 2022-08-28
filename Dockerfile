f8fc9d1bffffa9c81fbe16bff6d3be761d8ada03
tatianakruh <tatianakruh@ext.inditex.com>
2022-08-28 16:19:50 +0300

    Added Dockerfile and docker-compose.yaml

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:20.04
+
+RUN apt-get update -y && \
+    apt-get install -y python3.9 python3.9-dev && \
+    apt-get install -y python3-pip && \
+    apt-get install -y wget unzip && \
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
+    apt-get install -y ./google-chrome-stable_current_amd64.deb && \
+    wget https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip && \
+    unzip chromedriver_linux64.zip && \
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["behave"]
--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,28 @@
 from behave import fixture, use_fixture
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common import exceptions
+from selenium.webdriver.chrome.options import Options
 
 
 @fixture
 def browser_chrome(context):
-    context.browser = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("window-size=1024,768")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    context.browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     context.browser.maximize_window()
     context.browser.implicitly_wait(10)
     yield context.browser
     context.browser.quit()
+
+    try:
+        context.browser.get("https://www.google.com/")
+    except exceptions.InvalidSessionIdException as e:
+        print(e.msg)
 
 
 def before_all(context):
